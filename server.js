992533dc8f3ce5311b8136ce4e58bf0a3a65b547
matthi01 <mruhland01@gmail.com>
2018-04-17 14:02:27 -0700

    small error in the request logging

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ app.use((req, res, next) => {
   let now = new Date().toString();
   let log = `${now}: ${req.method} ${req.url}`;
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => console.log('Unable to append to server.log'));
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
   next();
 });
 
