5befb8d35e0079ceb8fcfad22b27ee41933e0ae1
matthi01 <mruhland01@gmail.com>
2018-04-16 20:35:12 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs')
+
+// 'next()' indicates when the middleware is done
+// add middleware to log any requests that are made, with a timestamp + write these to a log file
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => console.log('Unable to append to server.log'));
+  next();
+});
+
+// middleware to display maintenance page - not calling next() here
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
+// use .use to register express middleware
+app.use(express.static(__dirname + '/public'));
+
+//handler for http get request (takes url and function to run)
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'This is some dynamic welcome message'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({errorMessage: '<h1>something went wrong</h1>'});
+});
+
+app.listen(3000, () => console.log('Server is up on port 3000'));
